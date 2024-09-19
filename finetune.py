e175eb6688d3cbd4cf87a79796669f7ce2967206
deekshaaneja <deekshaaneja@gmail.com>
2024-09-20 00:18:15 +0530

    Added data

--- finetune.py
+++ finetune.py
@@ -325,6 +325,7 @@ def train():
         trust_remote_code=True,
     )
     tokenizer.pad_token_id = tokenizer.eod_id
+    tokenizer.pad_token_id = 151643
 
     if training_args.use_lora:
         if lora_args.q_lora or "chat" in model_args.model_name_or_path.lower():
