33a810c065aab71402f418033d84270400d120b5
deekshaaneja <deekshaaneja@gmail.com>
2024-09-20 21:44:13 +0530

    updated

--- scripts/finetune_qlora_single_gpu.sh
+++ scripts/finetune_qlora_single_gpu.sh
@@ -17,15 +17,15 @@ python finetune.py \
     --fix_vit True \
     --output_dir output_qwen \
     --num_train_epochs 5 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 1000 \
     --save_total_limit 10 \
     --learning_rate 1e-5 \
-    --weight_decay 0.1 \
+    --weight_decay 0.01 \
     --adam_beta2 0.95 \
     --warmup_ratio 0.01 \
     --lr_scheduler_type "cosine" \
